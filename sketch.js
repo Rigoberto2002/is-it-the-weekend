c4cb173fd645197b9c1e6cccad918573f3c61436
Rigoberto Ortega <rigobertoo56039@students.laalliance.org>
2017-11-17 20:27:31 +0000

    I'm done with my lab.

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var answer = 'NO'
+
+function setup(){
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw(){
+    background('dodgerblue')
+    textSize(32)
+    fill('white')
+    text('Is it the weekend ' + answer, 40, 40)
+}
